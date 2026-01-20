0d24aa3575736530d5d7da312499575ba450d059
Kenneth Ong <kenneth.ong3003@gmail.com>
2026-01-20 10:33:23 +0800

    Renamed class and added greetings

---
+++ src/main/java/Chatterbox.java
@@ -0,0 +1,10 @@
+public class Chatterbox {
+    public static void main(String[] args) {
+        String GREET_MSG = "Hello! I'm Chatterbox\n" +
+                            "What can I do for you?";
+        String BYE_MSG = "Bye! Hope to see you again soon!";
+        
+        System.out.println(GREET_MSG);
+        System.out.println(BYE_MSG);
+    }
+}
