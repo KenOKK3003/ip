41b8c0ababf06c25845315253040ed2ffb92a5ee
Kenneth Ong <kenneth.ong3003@gmail.com>
2026-01-20 16:37:56 +0800

    Added mark and unmark functionality

--- src/main/java/Chatterbox.java
+++ src/main/java/Chatterbox.java
@@ -1,6 +1,37 @@
 import java.util.Scanner;
 
 public class Chatterbox {
+    static class Task {
+        private String description;
+        private boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsNotDone() {
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "[X]" : "[ ]");
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return getStatusIcon() + " " + description;
+        }
+    }
+
     public static void main(String[] args) {
         String LINE = "________________________________";
         String GREET_MSG = " Hello! I'm Chatterbox\n" +
@@ -13,7 +44,7 @@ public class Chatterbox {
 
         Scanner scanner = new Scanner(System.in);
         String input;
-        String[] memory = new String[100];
+        Task[] memory = new Task[100];
         int memoryIndex = 0;
         
         while (true) {
@@ -28,16 +59,61 @@ public class Chatterbox {
             // Prints out memory if user types "list"
             if (input.equals("list")) {
                 System.out.println(LINE);
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < memory.length; i++) {
                     if (memory[i] != null) {
-                        System.out.println(i + 1 + ". " + memory[i]);
+                        System.out.println(" " + (i + 1) + "." + memory[i]);
                     }
                 }
                 System.out.println(LINE);
                 continue;
             }
+            // Marks task as done
+            if (input.startsWith("mark ")) {
+                try {
+                    int taskNum = Integer.parseInt(input.substring(5));
+                    if (taskNum > 0 && taskNum <= memoryIndex && memory[taskNum - 1] != null) {
+                        memory[taskNum - 1].markAsDone();
+                        System.out.println(LINE);
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   " + memory[taskNum - 1]);
+                        System.out.println(LINE);
+                    } else {
+                        System.out.println(LINE);
+                        System.out.println(" Invalid task number!");
+                        System.out.println(LINE);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(LINE);
+                    System.out.println(" Please provide a valid task number!");
+                    System.out.println(LINE);
+                }
+                continue;
+            }
+            // Marks task as not done
+            if (input.startsWith("unmark ")) {
+                try {
+                    int taskNum = Integer.parseInt(input.substring(7));
+                    if (taskNum > 0 && taskNum <= memoryIndex && memory[taskNum - 1] != null) {
+                        memory[taskNum - 1].markAsNotDone();
+                        System.out.println(LINE);
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("   " + memory[taskNum - 1]);
+                        System.out.println(LINE);
+                    } else {
+                        System.out.println(LINE);
+                        System.out.println(" Invalid task number!");
+                        System.out.println(LINE);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(LINE);
+                    System.out.println(" Please provide a valid task number!");
+                    System.out.println(LINE);
+                }
+                continue;
+            }
             // Stores input in memory
-            memory[memoryIndex] = input;
+            memory[memoryIndex] = new Task(input);
             System.out.println(LINE);
             System.out.print("added " + input + "\n");
             System.out.println(LINE);
