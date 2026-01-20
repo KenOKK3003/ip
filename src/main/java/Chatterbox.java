a32bd1a497e2e6b6ebddb27e48a202945a0c2867
Kenneth Ong <kenneth.ong3003@gmail.com>
2026-01-20 10:51:56 +0800

    Added echo capability and improved presentation

--- src/main/java/Chatterbox.java
+++ src/main/java/Chatterbox.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class Chatterbox {
     public static void main(String[] args) {
-        String GREET_MSG = "Hello! I'm Chatterbox\n" +
-                            "What can I do for you?";
-        String BYE_MSG = "Bye! Hope to see you again soon!";
+        String LINE = "________________________________";
+        String GREET_MSG = " Hello! I'm Chatterbox\n" +
+                            " What can I do for you?";
+        String BYE_MSG = " Bye! Hope to see you again soon!";
         
+        System.out.println(LINE);
         System.out.println(GREET_MSG);
+        System.out.println(LINE);
 
         Scanner scanner = new Scanner(System.in);
         String input;
@@ -14,10 +17,14 @@ public class Chatterbox {
         while (true) {
             input = scanner.nextLine();
             if (input.equals("bye")) {
+                System.out.println(LINE);
                 System.out.println(BYE_MSG);
+                System.out.println(LINE);
                 break;
             }
-            System.out.println("Chatterbox replies: " + input);
+            System.out.println(LINE);
+            System.out.println("Chatterbox echoes: " + input);
+            System.out.println(LINE);
         }
         
         scanner.close();
