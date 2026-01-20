3bc98d652a90e723374f0ca5526307aae5cdd360
Kenneth Ong <kenneth.ong3003@gmail.com>
2026-01-20 16:29:14 +0800

    Added add and list functionality

--- src/main/java/Chatterbox.java
+++ src/main/java/Chatterbox.java
@@ -13,20 +13,36 @@ public class Chatterbox {
 
         Scanner scanner = new Scanner(System.in);
         String input;
+        String[] memory = new String[100];
+        int memoryIndex = 0;
         
         while (true) {
             input = scanner.nextLine();
+            // Exits if user types "bye"
             if (input.equals("bye")) {
                 System.out.println(LINE);
                 System.out.println(BYE_MSG);
                 System.out.println(LINE);
                 break;
             }
+            // Prints out memory if user types "list"
+            if (input.equals("list")) {
+                System.out.println(LINE);
+                for (int i = 0; i < memory.length; i++) {
+                    if (memory[i] != null) {
+                        System.out.println(i + 1 + ". " + memory[i]);
+                    }
+                }
+                System.out.println(LINE);
+                continue;
+            }
+            // Stores input in memory
+            memory[memoryIndex] = input;
             System.out.println(LINE);
-            System.out.println("Chatterbox echoes: " + input);
+            System.out.print("added " + input + "\n");
             System.out.println(LINE);
+            memoryIndex++;
         }
-        
         scanner.close();
     }
 }
